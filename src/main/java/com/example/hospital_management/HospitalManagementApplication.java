9fda8d353a9f86b7f34c5f633a3af4d1ab435785
Othmane Boumya <boumyaothmane@gmail.com>
2024-04-21 16:18:32 +0100

    configured the InMemoryAuthentication

--- src/main/java/com/example/hospital_management/HospitalManagementApplication.java
+++ src/main/java/com/example/hospital_management/HospitalManagementApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -46,7 +49,10 @@ public class HospitalManagementApplication implements CommandLineRunner {
         patientRepository.save(patient);
         patientRepository.save(patient1);
         patientRepository.save(patient2);
+    }
 
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/hospital_management/web/Patientcontroller.java
+++ src/main/java/com/example/hospital_management/web/Patientcontroller.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @AllArgsConstructor
 public class Patientcontroller {
     private PatientRepository patientRepository;
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
                         @RequestParam(name = "size", defaultValue = "3") int size,
                         @RequestParam(name = "kw", defaultValue = "") String kw){
@@ -29,31 +30,41 @@ public class Patientcontroller {
         model.addAttribute("keyword", kw);
         return "patients";
     }
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String delete(Long id){
         patientRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/formPatients")
+    @GetMapping("/admin/formPatients")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+
     public String formPatients(Model model){
         model.addAttribute("patient", new Patient());
         return "formPatients";
     }
 
-    @PostMapping("/save")
+    @PostMapping("save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
         if (bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
         return "redirect:/index";
     }
 
-    @GetMapping("/editPatient")
+    @GetMapping("/admin/editPatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String editPatient(Model model, Long id){
         Patient patient = patientRepository.findById(id).orElse(null);
         if (patient == null) throw new RuntimeException("Patient Not Found");
         model.addAttribute("patient", patient);
         return "editPatient";
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/user/index";
     }
 
 

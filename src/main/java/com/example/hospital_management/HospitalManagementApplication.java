98dc9deb1060874adf5eeacb4d4f32ffa50e4be9
Othmane Boumya <boumyaothmane@gmail.com>
2024-04-21 17:45:51 +0100

    configured jdbc authentication

--- src/main/java/com/example/hospital_management/HospitalManagementApplication.java
+++ src/main/java/com/example/hospital_management/HospitalManagementApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -49,6 +51,22 @@ public class HospitalManagementApplication implements CommandLineRunner {
         patientRepository.save(patient);
         patientRepository.save(patient1);
         patientRepository.save(patient2);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return args -> {
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user12").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("ADMIN","USER").build()
+            );
+        };
     }
 
     @Bean
